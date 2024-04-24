37819518cab73ee09391281a994ca6abefbd9315
Requiem493 <akshaj.illa@gmail.com>
2024-04-24 19:04:59 -0400

    started swing

--- App.java
+++ App.java
@@ -1,7 +1,23 @@
 package Challenge2;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import java.awt.Color;
+
 public class App {
     public static void main(String[] args) {
-        
+        // create JFrame
+        JFrame frame = new JFrame("QBay");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // make it so when JFrame is closed via the x button, the entire program stops
+        frame.setLocationRelativeTo(null); // make JFrame open center screen
+        frame.setResizable(false);
+
+         // create main panel (canvas)
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(null); // no layout so you can put things wherever you want
+        mainPanel.setBackground(Color.blue);
     }
 }
