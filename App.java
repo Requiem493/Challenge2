6f5f368c417a20a73d6d2a6f5d63db67fae84aa9
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-04-24 19:01:25 -0400

    Added necessary functions

--- App.java
+++ App.java
@@ -2,4 +2,41 @@ public class App {
     public static void main(String[] args) {
         
     }
+
+    private static void initializeRepo() {
+
+    }
+
+    private static void createGitignore() {
+
+    }
+
+    private static void createReadMe() {
+
+    }
+
+    private static void createInitCommit() {
+
+    }
+
+    private static void createGitHubRepo() {
+
+    }
+
+    private static void setOrigin() {
+
+    }
+
+    private static void pushCommit() {
+
+    }
+
+    private static void displayURL() {
+
+    }
+
+    private static void getAPIKey() {
+        //Gets key from a file named "key.txt"
+        //File has been gitignored
+    }
 }
